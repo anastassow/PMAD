42a5455614432a6a320c85f73646fcaa97871756
anastassow <127148449+anastassow@users.noreply.github.com>
2026-02-11 15:10:43 +0200

    layout of the BlockHeader and SizeClass

---
+++ structures/BlockHeader.h
@@ -0,0 +1,8 @@
+#ifndef BLOCK_HEADER_H
+#define BLOCK_HEADER_H
+
+typedef struct BlockHeader {
+    
+} BlockHeader;
+
+#endif
---
+++ structures/SizeClass.h
@@ -0,0 +1,8 @@
+#ifndef SIZE_CLASS_H
+#define SIZR_CLASS_H
+
+typedef struct SizeClass {
+    
+} SizeClass;
+
+#endif
