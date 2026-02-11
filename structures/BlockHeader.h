dd083ef4dcf978d98ac3da95df09d13c91ea5b6b
anastassow <127148449+anastassow@users.noreply.github.com>
2026-02-11 22:10:13 +0200

    structures of the memory holding

--- structures/BlockHeader.h
+++ structures/BlockHeader.h
@@ -1,8 +1,11 @@
 #ifndef BLOCK_HEADER_H
 #define BLOCK_HEADER_H
 
+#include <stdint.h>
+
 typedef struct BlockHeader {
-    
+    struct BlockHeader* next;
+    uint8_t size_class;
 } BlockHeader;
 
 #endif
--- structures/SizeClass.h
+++ structures/SizeClass.h
@@ -1,8 +1,15 @@
 #ifndef SIZE_CLASS_H
 #define SIZR_CLASS_H
 
+#include "BlockHeader.h"
+#include <stddef.h>
+
 typedef struct SizeClass {
-    
+    size_t block_size;
+    BlockHeader* free_list;
+
+    size_t total_blocks;
+    size_t allocated_blocks;
 } SizeClass;
 
 #endif
