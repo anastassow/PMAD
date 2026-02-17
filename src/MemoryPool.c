b17e12be4a8f74fd1490a05fdb48cacc1abb568a
anastassow <127148449+anastassow@users.noreply.github.com>
2026-02-18 00:23:23 +0200

    to do for the function because now i made the algorith but i don't have the time to implement it now

--- src/MemoryPool.c
+++ src/MemoryPool.c
@@ -13,5 +13,19 @@ void attach_new_pool(PMAD* pmad, void* mem) {
 }
 
 void split_pool_by_percentage(struct PMAD* pmad, MemoryPool* pool, size_t percentage[NUM_CLASSES]){
-    
+    // TODO: Initialize a pointer to the start of the memory pool
+
+    // TODO: For each size class (i = 0 to NUM_CLASSES-1):
+        // TODO: Reset the free list of this size class
+        // TODO: Calculate the number of bytes allocated to this size class based on percentages[i]
+        // TODO: Calculate how many blocks of this size fit into the allocated bytes
+        // TODO: Set the total_blocks and allocated_blocks counters for this size class
+
+        // TODO: For each block in this size class:
+            // TODO: Cast the current pool pointer to a BlockHeader
+            // TODO: Set the block's size_class index
+            // TODO: Push the block to the head of the free list
+            // TODO: Advance the pool pointer by the block size
+
+    // TODO: Done — memory pool is now split and free lists are populated
 }
